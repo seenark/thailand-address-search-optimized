3e482ef8de7cbdf79c06d52669fdbd925d791397
Atiwat Seenark <um_oom@hotmail.com>
2023-09-14 13:56:23 +0700

    update: helper functions

--- src/address-helper.ts
+++ src/address-helper.ts
@@ -3,16 +3,24 @@ import { getProvinceByProvinceId } from "./province";
 import { getTambonByTambonId } from "./tambon";
 import { Amphure, Province, none, some, Option } from "./types";
 
-export function getAmphureAndProvinceByTambonId(
+export function getTambonAndAmphureAndProvinceByTambonId(
   id: number,
 ): Option<{ amphure: Amphure; province: Province }> {
   const tambonOption = getTambonByTambonId(id);
+
   if (tambonOption._tag === "None") return none();
+
   const amphureOption = getAmphureByAmphureId(tambonOption.value.amphure_id);
   if (amphureOption._tag === "None") return none();
+
   const provinceOption = getProvinceByProvinceId(
     amphureOption.value.province_id,
   );
   if (provinceOption._tag === "None") return none();
-  return some({ amphure: amphureOption.value, province: provinceOption.value });
+
+  return some({
+    tambon: tambonOption.value,
+    amphure: amphureOption.value,
+    province: provinceOption.value,
+  });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getAmphureAndProvinceByTambonId } from "./address-helper";
+import { getTambonAndAmphureAndProvinceByTambonId } from "./address-helper";
 import Zip from "./raw/zip_codes.json";
 import {
   getTambonListFromZipCodeGroup,
@@ -7,11 +7,6 @@ import {
 } from "./tambon";
 
 async function main() {
-  // const a = binarySearchByZipCode(Zip, "zip_code", 10100);
-  // console.log("a", a);
-  // extractZipCode();
-  // setOfZipCode();
-
   console.time("1");
   const tambons = getTambonsIdListFromZipCode(Zip, 10100);
   console.log("tambons", tambons);
@@ -27,7 +22,7 @@ async function main() {
   console.log("tambons3", tambons3);
   console.timeEnd("3");
 
-  const ap = getAmphureAndProvinceByTambonId(tambons[2].tumbon_id);
+  const ap = getTambonAndAmphureAndProvinceByTambonId(tambons[2].tumbon_id);
   console.log(ap);
 }
 
