872a48357d81f0ad67ad68f514654614b78f997f
Atiwat Seenark <um_oom@hotmail.com>
2023-09-14 15:39:10 +0700

    update: zipcode to tambon group

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,33 @@ import {
   getTambonListFromZipCodeTest,
   getTambonsIdListFromZipCode,
 } from "./tambon";
+import Tambons from "./raw/thai_tambons.json";
+import { Tambon } from "./types";
+
+function makeGroupForTambon() {
+  const map = new Map<string, Tambon[]>();
+  Tambons.RECORDS.forEach((t) => {
+    const value = {
+      id: t.id,
+      name_en: t.name_en,
+      name_th: t.name_th,
+      zip_code: t.zip_code,
+      amphure_id: t.amphure_id,
+    };
+    const key = `${t.zip_code}`;
+    const find = map.get(key);
+    if (!find) {
+      map.set(key, [value]);
+    } else {
+      map.set(key, [...find, value]);
+    }
+  });
+
+  // console.log("map", map);
+  const obj = Object.fromEntries(map);
+  console.log(obj);
+  // Bun.write("./zip_code_to_tambon_group.json", JSON.stringify(obj));
+}
 
 async function main() {
   console.time("1");
@@ -24,6 +51,8 @@ async function main() {
 
   const ap = getTambonAndAmphureAndProvinceByTambonId(tambons[2].tumbon_id);
   console.log(ap);
+
+  makeGroupForTambon();
 }
 
 main();
--- src/tambon.ts
+++ src/tambon.ts
@@ -1,7 +1,7 @@
 import { binarySearchByZipCode } from "./bs-filter";
 import { Option, Tambon, ZipCode, none, some } from "./types";
 import tambons from "./raw/thai_tambons.json";
-import ZipCodeGroup from "./raw/zip_code_group.json";
+import ZipCodeToTambonGroup from "./raw/zip_code_to_tambon_group.json";
 import Zip from "./raw/zip_codes.json";
 
 const tambonsMap = new Map<number, Tambon>(
@@ -27,7 +27,7 @@ export function getTambonByTambonId(id: number): Option<Tambon> {
 
 export function getTambonListFromZipCodeGroup(zipcode: number) {
   // const tp = await zipCodeGroupMap.get(zipcode)
-  const tp = ZipCodeGroup[zipcode] as Tambon[];
+  const tp = ZipCodeToTambonGroup[zipcode] as Tambon[];
   if (!tp) return none();
   return tp;
 }
